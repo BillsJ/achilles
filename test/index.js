8465f7cc43b8a6b5d818ace0ed32a681489fc39f
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-08-12 12:28:29 +0100

    Passes tests

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ var assert = require("assert");
 function Album(name) {
 	achilles.Object.call(this);
 	this.define("name", String);
+	this.define("rating", Number);
 	this.name = name;
 }
 
@@ -15,6 +16,18 @@ describe("achilles.Object.define", function() {
 		var album = new Album("Femme Fatale");
 		assert(album._type.name === String);
     });
+
+	it("should support type casting from String to Number", function() {
+		var album = new Album("Pixie Lott");
+		album.rating = "3";
+		assert(album.rating === 3);
+	});
+
+	it("should support type casting from Number to String", function() {
+		var album = new Album();
+		album.name = 3;
+		assert(album.name === "3");
+	});
 });
 
 describe("achilles.Object.toJSON", function() {
