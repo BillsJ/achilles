229ce9e62e3bea61d4730f35de41a508bfe8bfe9
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2014-08-21 15:17:35 +0100

    Furthered authentication/permissions framework

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ achilles.Object = require("./lib/Object");
 achilles.Model = require("./lib/Model");
 achilles.Router = require("./lib/Router");
 achilles.Service = require("./lib/Service");
+achilles.User = require("./lib/User");
+achilles.Group = require("./lib/Group");
 
 module.exports = achilles;
